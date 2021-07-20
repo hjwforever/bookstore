48682c9aea280cfc04e1ff011414be4341135d51
hjwforever <hjwbelieve@foxmail.com>
2021-07-20 11:11:32 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
 
-  if (req.url.startsWith('/api')) {
+  if (req.url.startsWith('/backend')) {
     target = 'https://bookstore-server.aruoxi.com'
   }
 
@@ -11,7 +11,7 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      '^/api/': '/'
+      '^/backend/': '/'
     }
   })(req, res)
 }
